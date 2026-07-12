e82661725e3853973eb0488bfa505e84c49047e8
Jonathan <ew15dro6k216@opayq.net>
2026-07-12 09:52:53 -0400

    Modify page-special and page-full-width-special to include custom HTML section.

--- coral-dark-child/page-full-width-special.php
+++ coral-dark-child/page-full-width-special.php
@@ -14,7 +14,33 @@ get_header(); ?>
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'content', 'page' ); ?>
+                                <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+                                        <header class="entry-header">
+                                                <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+                                        </header><!-- .entry-header -->
+
+				        <div class="entry-content special-page-content">
+
+				                <!-- custom HTML starts here -->
+
+
+
+
+						<p>some words here</p>
+
+
+
+
+
+
+
+
+						<!-- custom HTML ends here -->
+
+				        </div><!-- .entry-content -->
+
+				</article><!-- #post-<?php the_ID(); ?> -->
 
 				<?php
 					// If comments are open or we have at least one comment, load up the comment template
--- coral-dark-child/page-special.php
+++ coral-dark-child/page-special.php
@@ -14,7 +14,31 @@ get_header(); ?>
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'content', 'page' ); ?>
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+				        <div class="entry-content special-page-content">
+
+                                        <header class="entry-header">
+                                                <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+                                        </header><!-- .entry-header -->
+
+				                <!-- custom HTML starts here -->
+
+
+
+
+
+
+
+
+
+
+
+						<!-- custom HTML ends here -->
+
+				        </div><!-- .entry-content -->
+
+				</article><!-- #post-<?php the_ID(); ?> -->
 
 				<?php
 					// If comments are open or we have at least one comment, load up the comment template
