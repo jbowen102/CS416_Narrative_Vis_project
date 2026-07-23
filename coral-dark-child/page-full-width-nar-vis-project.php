54e094c8f1850f0526254b0a36cd402a536e129b
Jonathan <ew15dro6k216@opayq.net>
2026-07-23 05:43:52 -0400

    Create page for narrative vis project.

---
+++ coral-dark-child/page-full-width-nar-vis-project.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Template Name: Narrative Vis Project
+ * Custom template saved off coral-dark/page-full-width.php 2026-07-12.
+ *
+ * Created for use with D3 visualization project.
+ *
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area grid-100 tablet-grid-100 mobile-grid-100">
+		<main id="main" class="site-main" role="main">
+
+			<?php while ( have_posts() ) : the_post(); ?>
+
+                                <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+                                        <header class="entry-header">
+                                                <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+                                        </header><!-- .entry-header -->
+
+				        <div class="entry-content special-page-content">
+
+							<!-- custom HTML starts here -->
+
+							<p>to use with Narrative Vis project</p>
+
+							<script src='https://d3js.org/d3.v5.min.js'></script>
+
+							<svg width="400" height="400">
+
+							</svg>
+
+							<script>
+
+
+
+
+
+							</script>
+
+							<!-- custom HTML ends here -->
+
+				        </div><!-- .entry-content -->
+
+				</article><!-- #post-<?php the_ID(); ?> -->
+
+				<?php
+					// If comments are open or we have at least one comment, load up the comment template
+					if ( comments_open() || get_comments_number() ) :
+						comments_template();
+					endif;
+				?>
+
+			<?php endwhile; // end of the loop. ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
