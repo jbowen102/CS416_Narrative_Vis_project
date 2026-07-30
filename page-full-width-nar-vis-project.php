d04fdd0eae3350c4dd2275f02941f406a0d2cc61
Jonathan <ew15dro6k216@opayq.net>
2026-07-30 06:50:42 -0400

    Incorporate second species on plot, with check boxes for selecting either or both. Expand date range.

--- page-full-width-nar-vis-project.php
+++ page-full-width-nar-vis-project.php
@@ -29,96 +29,176 @@ get_header(); ?>
 
                 <div id="viz-wrapper">
 
-					<input id="week-slider" type="range" min="31" max="43" value="31">
-					<span id="week-label">Week 31</span>
-					</br>
-					<svg width="1200" height="1200">
+                    <input id="week-slider" type="range" min="27" max="48" value="27">
+					<span id="week-label">Week 27</span>
+
+                    <div id="species-pick">
+                        <label style="margin-right:12px;"><input type="checkbox" id="chk-stk" checked> Swallow-tailed Kite</label>
+                        <label><input type="checkbox" id="chk-bwh" checked> Broad-winged Hawk</label>
+                    </div>
+                    </br>
+                    <svg width="1200" height="1200">
 
 					</svg>
 
-					<script>
+                    <script>
 						window.addEventListener("load", init);
+						
+                        let margin = 70;
+                        let selectedWeek = 27;
+                        d3.select("#week-label").text(`Week ${selectedWeek}`);
+						
+                        let data_stk;
+                        let data_bwh;
+                        let chartLayer;
+                        let layerSTK;
+                        let layerBWH;
+                        const svg = d3.select("svg");
+						
+                        async function init() {
+							data_stk = await d3.csv("/data/CS416_NVis_project/swallow_tailed_kite_compl_exp_range_20260729_zf_clean_agg_weekly.csv", d3.autoType);
+                            data_bwh = await d3.csv("/data/CS416_NVis_project/broad_winged_hawk_compl_exp_range_20260729_zf_clean_agg_weekly.csv", d3.autoType);
 
-						let margin = 70;
-						let selectedWeek = 31;
-						d3.select("#week-label").text(`Week ${selectedWeek}`);
+                            const plot_height = 1000;
+                            const plot_width = 1000;
 
-						let stk_data;
-						let chartLayer;
-						const svg = d3.select("svg");
+                            const x = d3.scaleLinear()
+                                .domain([-109, -50])
+                                .range([0, plot_width]);
+                                // expanded longitude range by 1 degree West to un-crowd axis
+                                
+                                const y = d3.scaleLinear()
+                                .domain([-16, 40])
+                                .range([plot_height, 0]);// expanded latitude range by 1 degree South to un-crowd axis
 
-						async function init() {
-							stk_data = await d3.csv("/data/CS416_NVis_project/swallow_tailed_kite_compl_20260726_zf_clean_agg_weekly.csv", d3.autoType);
+                            chartLayer = svg.append("g")
+                                .attr("transform", "translate(" + margin + "," + margin + ")");
 
-							const plot_height = 1000;
-							const plot_width = 1000;
+                            // separate groups per species so both can be shown simultaneously
+                            layerSTK = chartLayer.append("g").attr("class", "layer-stk");
+                            layerBWH = chartLayer.append("g").attr("class", "layer-bwh");
 
-							const x = d3.scaleLinear()
-								.domain([-109, -50])        // expanded longitude range by 1 degree West to un-crowd axis
-								.range([0, plot_width]);
+                            // set up axes
+                            chartLayer.append("g")
+                                    .call(d3.axisLeft(y));
+                            chartLayer.append("g")
+                                    .attr("transform", "translate(0," + plot_height + ")")
+                                    .call(d3.axisBottom(x));
 
-							const y = d3.scaleLinear()
-								.domain([-16, 40])          // expanded latitude range by 1 degree South to un-crowd axis
-								.range([plot_height, 0]);
+                            d3.select("#week-slider")
+                            .on("input", function () { selectedWeek = +this.value;
+                                                        d3.select("#week-label").text(`Week ${selectedWeek}`);
+                                                        updateChart2(x, y);
+                                                        }
+                                );
 
-							chartLayer = svg.append("g")
-								.attr("transform", "translate(" + margin + "," + margin + ")");
+                            // update when species selection changes
+                            d3.selectAll("#chk-stk, #chk-bwh").on("change", function() {
+                                updateChart2(x, y);
+                            });
 
-							// set up axes
-							chartLayer.append("g")
-									.call(d3.axisLeft(y));
-							chartLayer.append("g")
-									.attr("transform", "translate(0," + plot_height + ")")
-									.call(d3.axisBottom(x));
+                            updateChart2(x, y);
 
-							d3.select("#week-slider")
-							.on("input", function () { selectedWeek = +this.value;
-														d3.select("#week-label").text(`Week ${selectedWeek}`);
-														updateChart(x, y);
-														}
-								);
-								// "this" refers to whatever DOM element triggered the event.
-								// "this.value" is the value of the slider (as str, so convert to number w/ +).
+                        }
 
-							updateChart(x, y);
+                        function updateChart(x, y) {
+                            // assemble weekData from selected species
+                            const stkChecked = d3.select("#chk-stk").property("checked");
+                            const bwhChecked = d3.select("#chk-bwh").property("checked");
 
-						}
+                            let weekData = [];
+                            if (stkChecked && data_stk) {
+                                weekData = weekData.concat(
+                                    data_stk.filter(d => Number(d.week) === selectedWeek)
+                                        .map(d => Object.assign({}, d, { species: 'stk' }))
+                                );
+                            }
+                            if (bwhChecked && data_bwh) {
+                                weekData = weekData.concat(
+                                    data_bwh.filter(d => Number(d.week) === selectedWeek)
+                                        .map(d => Object.assign({}, d, { species: 'bwh' }))
+                                );
+                            }
 
-						function updateChart(x, y) {
-							const weekData = stk_data.filter(d => Number(d.week) === selectedWeek);
+                            const circles = chartLayer.selectAll("circle")
+                                .data(weekData, d => d.species + "-" + d.cell);
 
-							const circles = chartLayer.selectAll("circle")
-								.data(weekData, d => d.cell);
+                            circles.exit()
+                                .transition()
+                                .duration(300)
+                                .style("opacity", 0)
+                                .remove();
 
-							circles.exit()
-								.transition()
-								.duration(300)
-								.style("opacity", 0)
-								.remove();
+                            const enterCircles = circles.enter()
+                                .append("circle")
+                                .attr("cx", d => x(d.cell_ctr_lon))
+                                .attr("cy", d => y(d.cell_ctr_lat))
+                                .attr("r", 4)
+                                .style("opacity", 0)
+                                .attr("stroke", "gainsboro")
+								.attr("stroke-opacity", 0.3)
+                                .attr("fill", "none");
 
-							const enterCircles = circles.enter()
-								.append("circle")
-								.attr("cx", d => x(d.cell_ctr_lon))
-								.attr("cy", d => y(d.cell_ctr_lat))
-								.attr("r", 4)
-								.style("opacity", 0)
-								.attr("stroke", "black")
-								.attr("fill", "white");
+                            enterCircles.merge(circles)
+                                .attr("cx", d => x(d.cell_ctr_lon))
+                                .attr("cy", d => y(d.cell_ctr_lat))
+                                .attr("r", 4)
+                                .transition()
+                                .duration(300)
+                                .style("opacity", 1)
+                                .attr("fill", d => d.det_freq > 0 ? (d.species === 'stk' ? "royalblue" : "orange") : "none")
+                                .attr("stroke", "gainsboro")
+								.attr("stroke-opacity", 0.3);
 
-							enterCircles.merge(circles)
-								.attr("cx", d => x(d.cell_ctr_lon))
-								.attr("cy", d => y(d.cell_ctr_lat))
-								.attr("r", 4)
-								.transition()
-								.duration(300)
-								.style("opacity", 1)
-								.attr("fill", d => d.det_freq > 0 ? "royalblue" : "white")
-								.attr("stroke", "black");
-						}
+                        }
+
+                        // Render each species in its own layer so both are visible
+                        function updateChart2(x, y) {
+                            const stkChecked = d3.select("#chk-stk").property("checked");
+                            const bwhChecked = d3.select("#chk-bwh").property("checked");
+
+                            const stkWeek = stkChecked && data_stk ? data_stk.filter(d => Number(d.week) === selectedWeek) : [];
+                            const bwhWeek = bwhChecked && data_bwh ? data_bwh.filter(d => Number(d.week) === selectedWeek) : [];
+
+                            // STK
+                            const stkSel = layerSTK.selectAll("circle").data(stkWeek, d => d.cell);
+                            stkSel.exit().transition().duration(300).style("opacity", 0).remove();
+                            const stkEnter = stkSel.enter().append("circle")
+                                .attr("cx", d => x(d.cell_ctr_lon))
+                                .attr("cy", d => y(d.cell_ctr_lat))
+                                .attr("r", 4)
+                                .style("opacity", 0)
+                                .attr("stroke", "gainsboro")
+								.attr("stroke-opacity", 0.3);
+                            stkEnter.merge(stkSel)
+                                .attr("cx", d => x(d.cell_ctr_lon))
+                                .attr("cy", d => y(d.cell_ctr_lat))
+                                .transition().duration(300)
+                                .style("opacity", 1)
+                                .attr("fill", d => d.det_freq > 0 ? "royalblue" : "none");
+
+                            // BWH
+                            const bwhSel = layerBWH.selectAll("circle").data(bwhWeek, d => d.cell);
+                            bwhSel.exit().transition().duration(300).style("opacity", 0).remove();
+                            const bwhEnter = bwhSel.enter().append("circle")
+                                .attr("cx", d => x(d.cell_ctr_lon))
+                                .attr("cy", d => y(d.cell_ctr_lat))
+                                .attr("r", 4)
+                                .style("opacity", 0)
+                                .attr("stroke", "gainsboro")
+								.attr("stroke-opacity", 0.3);
+                            bwhEnter.merge(bwhSel)
+                                .attr("cx", d => x(d.cell_ctr_lon))
+                                .attr("cy", d => y(d.cell_ctr_lat))
+                                .transition().duration(300)
+                                .style("opacity", 1)
+								.attr("stroke-opacity", 0.3)
+                                .attr("fill", d => d.det_freq > 0 ? "orange" : "none");
+                        }
 
 
 
-					</script>
+                    </script>
 
 				</div> <!-- viz-wrapper -->
 
